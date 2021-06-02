4ed0262943138288b35e8773fc0553089fecff9d
UDPN-zhangXueFeng <zhangxuefeng@reddatetech.com>
2021-06-02 17:55:55 +0800

    6.2 --layout

--- src/app/pages/main/nodes/node-list/node-list.component.ts
+++ src/app/pages/main/nodes/node-list/node-list.component.ts
@@ -7,7 +7,13 @@ import { AddComponent } from './component/add/add.component';
 import { Subscription } from 'rxjs';
 import { NodeListService } from './node-list.service';
 import { finalize } from 'rxjs/operators';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
+
+
+export interface Fruit {
+  name: string;
+  }
 export interface UserData {
   Name: string;
   Number: string;
@@ -22,7 +28,14 @@ export interface UserData {
 @Component({
   selector: 'app-node-list',
   templateUrl: './node-list.component.html',
-  styleUrls: ['./node-list.component.scss']
+  styleUrls: ['./node-list.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 
 export class NodeListComponent implements AfterViewInit  {
@@ -30,6 +43,8 @@ export class NodeListComponent implements AfterViewInit  {
   loginLoading = false;
   displayedColumns: string[] = ['Name', 'Number', 'NodeTitle', 'Currency','Rate','Address','Status','Operate'];
   dataSource: MatTableDataSource<UserData>;
+  expandedElement: UserData | null;
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   constructor(
@@ -55,13 +70,13 @@ export class NodeListComponent implements AfterViewInit  {
     this.getList() 
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-        if (this.dataSource.paginator) {
-        this.dataSource.paginator.firstPage();
-        }
-    }
+  // applyFilter(event: Event) {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+  //       if (this.dataSource.paginator) {
+  //       this.dataSource.paginator.firstPage();
+  //       }
+  //   }
     
     openDialog(){
       const dialogRef = this.dialog.open(AddComponent,{width:'35%'});
@@ -69,5 +84,8 @@ export class NodeListComponent implements AfterViewInit  {
       console.log(`Dialog result: ${result}`);
       });
     }
-  }
+    del(event){
+      event.stopPropagation();
+    }
+}
 
